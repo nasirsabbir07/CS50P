2e2ffb9b6db2ec4c740561b851ecd92c96451829
Sabbir <61047782+nasirsabbir07@users.noreply.github.com>
2023-09-23 02:14:12 +0600

    Create nutrition.py

---
+++ problemset2/Nutrition_Facts/nutrition.py
@@ -0,0 +1,36 @@
+def main():
+    fruit_name = input("Item: ").lower()
+
+    calories(fruit_name)
+
+def calories(s):
+
+    calories = {
+            "apple": 130,
+            "avocado": 50,
+            "banana": 110,
+            "cantaloupe": 50,
+            "grapefruit": 60,
+            "grapes": 90,
+            "honeydew melon": 50,
+            "kiwifruit": 90,
+            "lemon": 15,
+            "lime": 20,
+            "nectarine": 60,
+            "orange": 80,
+            "peach": 60,
+            "pear": 100,
+            "pineaple": 50,
+            "plums": 70,
+            "strawberries": 50,
+            "sweet cherries": 100,
+            "tangerine": 50,
+            "watermelon": 80
+        }
+
+    if s in calories:
+       print(f"Calories: {calories[s]}")
+    else:
+        print("")
+
+main()
